5abf4b6901942b438d4937511a8527bfa7d75dcd
haka_12 <bakalantern@gmail.com>
2020-12-23 15:13:45 +0545

    code refactored

---
+++ main.py
@@ -0,0 +1,11 @@
+from face_detect import FaceDetect
+from eigen_pca import Eigen
+from test import Test
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import os
+
+
+class Test:
+    def __init__(self):
+        pass
